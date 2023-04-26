060e06a00b0335de2650b053db0a8d5c75b65354
Hamed Hosseini <hamed8080@gmail.com>
2023-04-26 20:50:16 +0330

    First commit

---
+++ Sources/ChatModels/ChatModels.docc/ChatModels.md
@@ -0,0 +1,4 @@
+# ``ChatModels``
+ChatModels consist of all base calsses in whih a client can have an interaction with them.
+
+![icon](icon.png)
---
+++ Tests/ChatModelsTests/ChatModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  ChatModelTests.swift
+//  
+//
+//  Created by hamed on 4/26/23.
+//
+
+import XCTest
+
+final class ChatModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    func test_method() {
+        XCTAssertTrue(true)
+    }
+}
